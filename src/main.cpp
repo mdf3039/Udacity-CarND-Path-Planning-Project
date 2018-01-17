c1c2d184d3d7028b210e8213cc688c1f63407b59
mdf3039 <mdf3039@gmail.com>
2018-01-17 07:50:30 -0600

    7th - fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -207,7 +207,7 @@ int main() {
   //Have a reference velocity to target
   double ref_vel = 49.5; //mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
