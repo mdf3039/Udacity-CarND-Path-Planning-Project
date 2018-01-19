bcc5e6b2e2a387c99c5b0b321b75ffe3f5301614
mdf3039 <mdf3039@gmail.com>
2018-01-18 20:25:15 -0600

    38th - fixed bug

--- src/main.cpp
+++ src/main.cpp
@@ -217,7 +217,7 @@ int main() {
   vector<double> prev_s;
   vector<double> prev_d;
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel,&need_lane_change,&counter,&prev_sd](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel,&need_lane_change,&counter,&prev_s,&prev_d](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
